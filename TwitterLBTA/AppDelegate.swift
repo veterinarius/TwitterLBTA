687c1f9932addba1b3ecf3fc98db205c4d5255f2
veterinarius <veterinarius@gmx.net>
2017-01-02 21:09:15 +0100

    LBTA2

--- TwitterLBTA/AppDelegate.swift
+++ TwitterLBTA/AppDelegate.swift
@@ -20,8 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
-        window?.rootViewController = UINavigationController(rootViewController: homeController)
+ //       let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+ //       window?.rootViewController = UINavigationController(rootViewController: homeController)
+        
+        window?.rootViewController = UINavigationController(rootViewController: HomedatasourceController())
         
          return true
     }
---
+++ TwitterLBTA/HomeDatasourceController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeDatasourceController.swift
+//  TwitterLBTA
+//
+//  Created by Stephan Wegmann on 02.01.17.
+//  Copyright © 2017 Stephan Wegmann. All rights reserved.
+//
+
+import LBTAComponents
+
+
+
+class HomedatasourceController: DatasourceController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
