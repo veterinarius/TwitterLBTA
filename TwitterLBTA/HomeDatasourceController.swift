a92f1b9aca11aa8b9e2f5e19195b611cb9fce57b
veterinarius <veterinarius@gmx.net>
2017-01-03 20:28:50 +0100

    LBTA3

--- TwitterLBTA/HomeDatasourceController.swift
+++ TwitterLBTA/HomeDatasourceController.swift
@@ -18,6 +18,10 @@ class HomedatasourceController: DatasourceController {
         self.datasource = homeDatasource
     }
     
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
