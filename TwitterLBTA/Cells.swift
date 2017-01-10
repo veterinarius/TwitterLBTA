05368aa621b533c00a69941c3fc185b9c9fd4bca
veterinarius <veterinarius@gmx.net>
2017-01-10 22:15:16 +0100

    LBTA5.1

--- TwitterLBTA/Cells.swift
+++ TwitterLBTA/Cells.swift
@@ -8,7 +8,6 @@
 
 import LBTAComponents
 
-
 let twitterBlue = UIColor(r: 61, g:167, b:244)
 
 class UserFooter: DatasourceCell {
@@ -40,86 +39,14 @@ class UserHeader: DatasourceCell {
     override func setupViews() {
         super.setupViews()
         
+        separatorLineView.isHidden = false
+        separatorLineView.backgroundColor = UIColor(r: 230, g: 230, b: 230)
+        
         addSubview(textLabel)
-        textLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        textLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 14, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
         
     }
 }
 
-class UserCell: DatasourceCell {
-    
-    override var datasourceItem: Any? {
-        didSet {
-            nameLabel.text = datasourceItem as? String
-        }
-    }
-    
-    let profileImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "aston")
-        imageView.layer.cornerRadius = 5
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
-    
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Stephan Wegmann"
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        return label
-        
-    }()
-    
-    let usernameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "@articYoulate"
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.textColor = UIColor(r: 130, g: 130, b:130)
-        return label
 
-    }()
-    
-    let bioTextView: UITextView = {
-        let textView = UITextView()
-        textView.text = "Hier lernst du auf einfache Weise Apps zu programmieren!"
-        textView.font = UIFont.systemFont(ofSize: 15)
-        textView.backgroundColor = .clear
-        return textView
-    }()
-    
-    let followButton: UIButton = {
-        let button = UIButton()
-        button.layer.cornerRadius = 5
-        button.layer.borderColor = twitterBlue.cgColor
-        button.layer.borderWidth = 1
-        button.setTitle("Folge mir!", for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14) 
-        button.setTitleColor(twitterBlue, for: .normal)
-        button.setImage(#imageLiteral(resourceName: "twitter-icon"), for: .normal)
-        button.imageView?.contentMode = .scaleAspectFit
-        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -8, bottom: 0, right: 0)
-        return button
-    }()
-    
-    override func setupViews() {
-        super.setupViews()
-        
-        addSubview(profileImageView)
-        addSubview(nameLabel)
-        addSubview(usernameLabel)
-        addSubview(bioTextView)
-        addSubview(followButton)
-        
-        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
-        
-        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
-        
-        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
-        
-        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: -4, leftConstant: -4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
-        
-        followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
-        
-    }
-}
 
---
+++ TwitterLBTA/UserCell.swift
@@ -0,0 +1,97 @@
+//
+//  UserCell.swift
+//  TwitterLBTA
+//
+//  Created by Stephan Wegmann on 10.01.17.
+//  Copyright © 2017 Stephan Wegmann. All rights reserved.
+//
+
+import LBTAComponents
+
+class UserCell: DatasourceCell {
+    
+    override var datasourceItem: Any! {
+        didSet {
+            print(datasourceItem)
+            
+            guard let user = datasourceItem as? User else {
+                return
+            }
+            nameLabel.text = user.name
+            usernameLabel.text = user.username
+            bioTextView.text = user.bioText
+            profileImageView.image = user.profileImage
+        }
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "aston")
+        imageView.layer.cornerRadius = 5
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Stephan Wegmann"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        return label
+        
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@articYoulate"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor(r: 130, g: 130, b:130)
+        return label
+        
+    }()
+    
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Hier lernst du auf einfache Weise Apps zu programmieren!"
+        textView.font = UIFont.systemFont(ofSize: 15)
+        textView.backgroundColor = .clear
+        return textView
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 5
+        button.layer.borderColor = twitterBlue.cgColor
+        button.layer.borderWidth = 1
+        button.setTitle("Folge mir!", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(twitterBlue, for: .normal)
+        //button.setImage(#imageLiteral(resourceName: "twitter-icon"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -8, bottom: 0, right: 0)
+        return button
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        separatorLineView.isHidden = false
+        separatorLineView.backgroundColor = UIColor(r: 230, g: 230, b: 230)
+        
+        addSubview(profileImageView)
+        addSubview(nameLabel)
+        addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
+        
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        
+        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: -4, leftConstant: -4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 100, heightConstant: 34)
+        
+}
+}
