7a6ae2a694c39e671a86bc5ff60ddfb0ac586d63
veterinarius <veterinarius@gmx.net>
2017-02-07 20:30:40 +0100

    Add files via upload

---
+++ HomeDatasource.swift
@@ -0,0 +1,72 @@
+//
+//  HomeDatasource.swift
+//  TwitterLBTA
+//
+//  Created by Brian Voong on 12/31/16.
+//  Copyright © 2016 Lets Build That App. All rights reserved.
+//
+
+import LBTAComponents
+import TRON
+import SwiftyJSON
+
+class HomeDatasource: Datasource, JSONDecodable {
+    
+    let users: [User]
+    
+    required init(json: JSON) throws {
+        var users = [User]()
+        
+        let array = json["users"].array
+        for userJson in array! {
+            let name = userJson["name"].stringValue
+            let username = userJson["username"].stringValue
+            let bio = userJson["bio"].stringValue
+            
+            let user = User(name: name, username: username, bioText: bio, profileImage: UIImage())
+            users.append(user)
+        }
+        
+        self.users = users
+    }
+    
+    let tweets: [Tweet] = {
+        let brianUser = User(name: "Brian Voong", username: "@buildthatapp", bioText: "iPhone, iPad, iOS Programming Community. Join us to learn Swift, Objective-C and build iOS apps!", profileImage: #imageLiteral(resourceName: "profile_image"))
+        let tweet = Tweet(user: brianUser, message: "Welcome to episode 9 of the Twitter Series, really hope you guys are enjoying the series so far. I really really need a long text block to render out so we're going to stop here.")
+        
+        let tweet2 = Tweet(user: brianUser, message: "This is the second tweet message for our sample project. Very very exciting message....")
+        
+        return [tweet, tweet2]
+    }()
+    
+    override func footerClasses() -> [DatasourceCell.Type]? {
+        return [UserFooter.self]
+    }
+    
+    override func headerClasses() -> [DatasourceCell.Type]? {
+        return [UserHeader.self]
+    }
+    
+    override func cellClasses() -> [DatasourceCell.Type] {
+        return [UserCell.self, TweetCell.self]
+    }
+    
+    override func item(_ indexPath: IndexPath) -> Any? {
+        if indexPath.section == 1 {
+            return tweets[indexPath.item]
+        }
+        return users[indexPath.item]
+    }
+    
+    override func numberOfSections() -> Int {
+        return 2
+    }
+    
+    override func numberOfItems(_ section: Int) -> Int {
+        if section == 1 {
+            return tweets.count
+        }
+        return users.count
+    }
+    
+}
---
+++ HomeDatasourceController.swift
@@ -0,0 +1,73 @@
+//
+//  HomeDatasourceController.swift
+//  TwitterLBTA
+//
+//  Created by Brian Voong on 12/31/16.
+//  Copyright © 2016 Lets Build That App. All rights reserved.
+//
+
+import LBTAComponents
+import TRON
+import SwiftyJSON
+
+class HomeDatasourceController: DatasourceController {
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionViewLayout.invalidateLayout()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor(r: 232, g: 236, b: 241)
+        
+        setupNavigationBarItems()
+        
+//        let homeDatasource = HomeDatasource()
+//        self.datasource = homeDatasource
+        
+//        fetchHomeFeed()
+        print(1)
+        Service.sharedInstance.fetchHomeFeed { (homeDatasource) in
+            print(3)
+            self.datasource = homeDatasource
+        }
+     }
+     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if let user = self.datasource?.item(indexPath) as? User {
+            
+            //let's get an estimation of the height of our cell based on user.bioText
+            
+            let approximateWidthOfBioTextView = view.frame.width - 12 - 50 - 12 - 2
+            let size = CGSize(width: approximateWidthOfBioTextView, height: 1000)
+            let attributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+            
+            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+            
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        }
+        
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 1 {
+            return .zero
+        }
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if section == 1 {
+            return .zero
+        }
+        return CGSize(width: view.frame.width, height: 64)
+    }
+    
+}
